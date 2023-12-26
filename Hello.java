7d5ba2885fa41935c9f4174284778d1951db6cae
jinsi <jinsi.viradiya24@gmail.com>
2023-12-26 16:11:32 +0530

    update lambda

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Lambda!");
+    public String handleRequest(String input) {
+        return "Hello, Lambda!";
     }
 }
